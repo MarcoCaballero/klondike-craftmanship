6a19506e46d4191a65bd0e0fd455e4276e0f2ccc
elliotAlderson <marco.caballero.d@gmail.com>
2017-10-16 23:15:04 +0200

    Adding task.json and c_cpp_properties to launch build from batch file (build.bat) added too

---
+++ build.bat
@@ -0,0 +1,16 @@
+REM Automated build cpp files with visual c++ tools (look task.json -> name:'build')
+@echo off
+REM Compiling..... <name>.cpp -> <name>.obj
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
+set compilerflags=/EHsc /c
+set objectsCPP= *.cpp
+cl.exe %compilerflags% %objectsCPP%
+REM Linking..... *.obj -> klondike.exe
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
+set linkerflags=/link /out:bin/klondike.exe
+set objectsOBJ= *.obj
+cl.exe  %objectsOBJ% %linkerflags%
+REM Removing *.obj .....
+call del *.obj
+pause
+exit
